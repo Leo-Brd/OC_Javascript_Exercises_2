8c1a9b33d9b3d7d19b2dbdc2d90dbb512257d981
Leo-Brd <leobordet.pro@gmail.com>
2025-12-05 18:50:36 +0100

    feat: add exercise 1

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,12 @@
-//votre code ici
+
+function pairNumbers(number1, number2) {
+    let pairs = "";
+    for (let i = number1; i <= number2; i++) {
+        if (i % 2 === 0) {
+            pairs += i + ",";
+        }
+    }
+    return pairs.slice(0, -1);
+}
 
 export default pairNumbers
